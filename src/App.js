529df2270c092dab3ca8689439cc0d938866a6ed
Alexandra Baumann <alexbaumann342@gmail.com>
2023-07-18 16:53:48 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+//importer toutes les pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Rental from "./pages/Rental";
+import Error from "./pages/Error";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path="/About" element={ <About /> } />
+        <Route path="/Rental/:id" element={ <Rental /> } />
+        <Route path="*" element={ <Error /> } />
+      </Routes>
     </div>
   );
 }
+// <Route path='/nomduchemin' element={ <nomdelapage /> } />
 
 export default App;
